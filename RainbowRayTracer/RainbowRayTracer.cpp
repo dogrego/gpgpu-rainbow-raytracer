353b2a98e7b385e8a8eb4b6c6b65b10c23af04ae
Grego <doragregorics@gmail.com>
2025-03-23 20:27:02 +0100

    Add initial `Hello World`

---
+++ RainbowRayTracer/RainbowRayTracer.cpp
@@ -0,0 +1,12 @@
+﻿// RainbowRayTracer.cpp : Defines the entry point for the application.
+//
+
+#include "RainbowRayTracer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
